5769f52a1296dc115bc384552d8a0ff5aedcfe93
SabriNouira <sabrinouira2@gmail.com>
2023-11-30 11:36:36 +0100

    .io

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -4,13 +4,12 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router){}
-
-  login(){
-    window.location.href = 'http://app.tracksales.dev.sync.tn';
+  login() {
+    window.location.href = 'http://app.tracksales.io';
   }
 }
