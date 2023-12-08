b9975ab5ca60c995b300a1e09f2edb8f4ecd00d4
SabriNouira <sabrinouira2@gmail.com>
2023-12-08 10:13:46 +0100

    eviter auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', children: [
-    {
-      path: '', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule)
-    },
-    {
-      path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-    }
-  ]} 
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./landing-page/landing-page.module').then(
+            (m) => m.LandingPageModule
+          ),
+      },
+    ],
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
