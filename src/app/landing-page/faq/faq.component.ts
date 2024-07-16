1b6299530f9345e273b08e821ec24c8913bc2dc6
SabriNouira <sabrinouira2@gmail.com>
2024-07-16 08:30:43 +0100

    transaction with navigateur language

--- src/app/landing-page/faq/faq.component.ts
+++ src/app/landing-page/faq/faq.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { ScrollService } from 'src/app/services/scroll.service';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
-  styleUrls: ['./faq.component.scss']
+  styleUrls: ['./faq.component.scss'],
 })
 export class FaqComponent {
-
+  constructor(private scrollService: ScrollService) {
+    this.scrollService.scrollToTop();
+  }
 }
